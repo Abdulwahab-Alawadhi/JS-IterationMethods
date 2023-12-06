abc6cf4e4db6dc002eb859db5c2fd26fc28c02fb
Abdulwahab-Alawadhi <abdulwahab.alawadhi56@gmail.com>
2023-12-06 20:23:34 +0300

    done

--- JS-IterationMethods/index.js
+++ JS-IterationMethods/index.js
@@ -33,8 +33,15 @@ function filterGreaterOrE(x) {
   } else {
   }
 }
-
 console.log(numbers.filter(filterGreaterOrE));
+
+function filterDivsBy5(x) {
+  if (x % 5 == 0) {
+    return x;
+  } else {
+  }
+}
+console.log(numbers.filter(filterDivsBy5));
 
 /*****************************************************************
 Part 2: Mapping
@@ -44,6 +51,16 @@ Write code to perform the following tasks using the map method:
 Q3) Create a new array that contains each number squared.
 Q4) Create a new array that contains each number multiplied by 2.
 ******************************************************************/
+//q3
+function mapSquared(x) {
+  return x * x;
+}
+console.log(numbers.map(mapSquared));
+//q4
+function mapDoubled(x) {
+  return x * 2;
+}
+console.log(numbers.map(mapDoubled));
 
 /*****************************************************************
 Part 3: Combining Filtering and Mapping
@@ -53,3 +70,14 @@ Combine the filter and map methods to perform the following tasks:
 Q5) Filter the numbers that are greater than or equal to 20 and then square each of them.
 Q6) Filter the numbers that are divisible by 5 and then multiply each of them by 3.
 ******************************************************************/
+//q5
+
+console.log(numbers.filter(filterGreaterOrE).map(mapSquared));
+
+//q6
+
+function mapTripled(x) {
+  return x * 3;
+}
+
+console.log(numbers.filter(filterDivsBy5).map(mapTripled));
